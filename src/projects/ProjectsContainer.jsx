7b9d8b82916c779a63bb397daddf033d24099ebf
DakotaLMartinez <dakotaleemusic@gmail.com>
2023-01-18 21:14:58 -0800

    initial commit

---
+++ src/projects/ProjectsContainer.jsx
@@ -0,0 +1,14 @@
+import React     from 'react'
+import PropTypes from 'prop-types'
+import { Routes, Route } from 'react-router-dom'
+
+const ProjectsContainer = ({ args }) =>
+  <Routes>
+    
+  </Routes>
+
+ProjectsContainer.propTypes = {
+  
+}
+
+export default ProjectsContainer
