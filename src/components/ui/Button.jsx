f418345831c92808664fb0148c58f76a697056fa
DakotaLMartinez <dakotaleemusic@gmail.com>
2023-02-18 15:46:14 -0800

    tweak Button for extensibility

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -3,6 +3,7 @@ import classNames from "classnames";
 function Button({
   children,
   rounded,
+  round,
   outline,
   primary,
   secondary,
@@ -10,23 +11,25 @@ function Button({
   warning,
   danger,
   className,
+  as: Tag="button",
   ...rest
 }) {
   const classes = classNames(
-    "flex items-center px-3 py-1 border",
+    "flex items-center px-3 py-1 border transition",
     {
       "border-slate-500": primary,
       "border-gray-900": secondary,
       "border-green-500": success,
       "border-yellow-400": warning,
       "border-red-500": danger,
-      "bg-slate-500 text-white": primary && !outline,
+      "bg-slate-500 hover:bg-slate-600 text-white": primary && !outline,
       "bg-gray-900 text-white": secondary && !outline,
       "bg-green-500 text-white": success && !outline,
       "bg-yellow-400 text-white": warning && !outline,
       "bg-red-500 text-white": danger && !outline,
-      "rounded-full": rounded,
-      "bg-white": outline,
+      "rounded-full": round,
+      "rounded-lg": rounded,
+      "bg-white hover:bg-slate-100": outline,
       "text-slate-500": outline && primary,
       "text-gray-900": outline && secondary,
       "text-green-500": outline && success,
@@ -36,9 +39,9 @@ function Button({
     className
   );
   return (
-    <button className={classes} {...rest}>
+    <Tag className={classes} {...rest}>
       {children}
-    </button>
+    </Tag>
   );
 }
 
