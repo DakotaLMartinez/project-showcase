3a64985407c88df697215768d9424b9846fa4812
DakotaLMartinez <dakotaleemusic@gmail.com>
2023-02-19 21:30:43 -0800

    tweak image styles for project card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,28 +5,51 @@ import Button from "./ui/Button";
 import GitHubLink from "./ui/GitHubLink";
 
 function ProjectCard({ project, small = false }) {
-  const {
-    id,
-    name,
-    about,
-    code_url,
-    featured_image_url,
-  } = project;
+  const { id, name, about, code_url, featured_image_url } = project;
 
   const aboutClasses = classNames("mt-2 whitespace-normal", {
     "sm:h-12 md:h-20": small,
     "sm:h-16 md:h-20": !small,
   });
 
-  const shortAboutText = about.slice(0,small ? 74 : 84);
+  const placeHolderImageClasses = classNames("max-w-full py-4 object-cover", {
+    "px-2 md:px-0 md:h-40 lg:h-44": small,
+    "sm:h-56 md:h-44 lg:h-52": !small,
+  });
+
+  const featuredImageClasses = classNames("object-cover", {
+    "md:h-40 lg:h-44": small,
+    "sm:h-56 md:h-44 lg:h-52": !small,
+  });
+
+  const shortAboutText = about.slice(0, small ? 74 : 84);
   const aboutText =
     shortAboutText === about ? about : `${shortAboutText.trim()}...`;
+
+  const renderPictureOrFallback = (imgUrl) => {
+    return imgUrl ? renderFeaturedImage(imgUrl) : renderPlaceholder();
+  };
+
+  const renderPlaceholder = () => {
+    return (
+      <img
+        className={placeHolderImageClasses}
+        src="https://via.placeholder.com/850x450?text=No%20Image"
+      />
+    );
+  };
+
+  const renderFeaturedImage = (imgUrl) => {
+    return (
+      <img className={featuredImageClasses} src={imgUrl} alt="Featured Image" />
+    );
+  };
 
   return (
     <article className="border-slate-50 border rounded-lg shadow-black shadow-md bg-slate-900 text-slate-100">
       <div className="px-4 pt-2">
-        <figure>
-          <img src={featured_image_url} />
+        <figure className="flex justify-center">
+          {renderPictureOrFallback(featured_image_url)}
         </figure>
         <h4 className="md:text-lg lg:text-xl border-b w-full">{name}</h4>
         <p className={aboutClasses}>{aboutText}</p>
