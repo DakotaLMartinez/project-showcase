ebb50efa1f22150bc85b62e670a078a963ab3f80
DakotaLMartinez <dakotaleemusic@gmail.com>
2023-03-28 18:16:08 -0700

    adjust element spacing on HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,7 +8,7 @@ function HomePage() {
   const { isLoggedIn } = useAuth();
   return (
     <div className="py-4 sm:py-8 mt-6">
-      <div className="min-h-80v grid text-center items-center justify-center py-4 px-8">
+      <div className="min-h-80v grid text-center items-center justify-center pb-16 sm:py-4 px-8">
         <h1 className="text-5xl">Showcase Your Best Work as a Developer</h1>
         <section className="mt-6 grid gap-8 md:grid-cols-3 px-0">
           <div className="md:col-span-2 flex items-center justify-center text-3xl">
@@ -19,17 +19,25 @@ function HomePage() {
           </div>
           <div>
             <figure className="flex justify-center">
-              <img src="https://res.cloudinary.com/dnocv6uwb/image/upload/c_scale,h_300/v1679005000/project-showcase-app/DALL_E_2023-03-16_15.15.06_-_Generate_an_image_of_a_young_woman_in_intricately_decorated_wizard_s_robes_with_purple_coloring_stars_and_silver_streaks._Cinematic_lighting._Woman_l.png" />
+              <img
+                src="https://res.cloudinary.com/dnocv6uwb/image/upload/c_scale,h_300/v1679005000/project-showcase-app/DALL_E_2023-03-16_15.15.06_-_Generate_an_image_of_a_young_woman_in_intricately_decorated_wizard_s_robes_with_purple_coloring_stars_and_silver_streaks._Cinematic_lighting._Woman_l.png"
+                width="300"
+                height="300"
+              />
             </figure>
           </div>
         </section>
       </div>
-      <div className="bg-black min-h-85v grid text-center items-center justify-center py-4 px-8">
+      <div className="bg-black min-h-85v grid text-center items-center justify-center py-16 sm:py-4 px-8">
         <h1 className="text-5xl">Create Your Developer Profile</h1>
         <section className="mt-6 grid gap-8 md:grid-cols-3 px-0">
           <div>
-            <figure className="">
-              <img src="https://res.cloudinary.com/dnocv6uwb/image/upload/c_scale,w_334/v1679008890/project-showcase-app/DALL_E_2023-03-16_16.19.24_-_Generate_an_image_of_a_young_woman_in_intricately_decorated_wizard_s_robes_with_purple_coloring_stars_and_silver_streaks._Cinematic_lighting._Woman_l.png" />
+            <figure className="flex justify-center">
+              <img
+                src="https://res.cloudinary.com/dnocv6uwb/image/upload/c_scale,w_334/v1679008890/project-showcase-app/DALL_E_2023-03-16_16.19.24_-_Generate_an_image_of_a_young_woman_in_intricately_decorated_wizard_s_robes_with_purple_coloring_stars_and_silver_streaks._Cinematic_lighting._Woman_l.png"
+                width="334"
+                height="334"
+              />
             </figure>
           </div>
           <div className="md:col-span-2 flex items-center justify-center text-3xl">
@@ -39,7 +47,7 @@ function HomePage() {
           </div>
         </section>
       </div>
-      <div className="min-h-85v grid text-center items-center justify-center py-4 px-8">
+      <div className="min-h-85v grid text-center items-center justify-center py-16 sm:py-4 px-8">
         <h1 className="text-5xl">Showcase Your Projects</h1>
         <section className="mt-6 grid gap-8 md:grid-cols-3 px-0">
           <div className="md:col-span-2 flex items-center justify-center text-3xl">
@@ -49,8 +57,12 @@ function HomePage() {
             building your projects.
           </div>
           <div>
-            <figure>
-              <img src="https://res.cloudinary.com/dnocv6uwb/image/upload/c_scale,w_334/v1679008890/project-showcase-app/DALL_E_2023-03-16_16.19.13_-_Generate_an_image_of_a_young_woman_in_intricately_decorated_wizard_s_robes_with_purple_coloring_stars_and_silver_streaks._Cinematic_lighting._Woman_l.png" />
+            <figure className="flex justify-center">
+              <img
+                src="https://res.cloudinary.com/dnocv6uwb/image/upload/c_scale,w_334/v1679008890/project-showcase-app/DALL_E_2023-03-16_16.19.13_-_Generate_an_image_of_a_young_woman_in_intricately_decorated_wizard_s_robes_with_purple_coloring_stars_and_silver_streaks._Cinematic_lighting._Woman_l.png"
+                width="334"
+                height="334"
+              />
             </figure>
           </div>
         </section>
